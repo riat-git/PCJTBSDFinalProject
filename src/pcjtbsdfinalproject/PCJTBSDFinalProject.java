86d12f5f7e09e463b7d5a3bbd1434feff6530db7
RIAT <nvin.dinuka@gmail.com>
2014-03-20 14:43:49 +0530

    Done commit

--- src/pcjtbsdfinalproject/PCJTBSDFinalProject.java
+++ src/pcjtbsdfinalproject/PCJTBSDFinalProject.java
@@ -17,6 +17,7 @@ public class PCJTBSDFinalProject {
      */
     public static void main(String[] args) {
         System.out.println("Git clone test ok!!!");
+        System.out.println("Done!!!!!!!!!");
     }
     
 }
